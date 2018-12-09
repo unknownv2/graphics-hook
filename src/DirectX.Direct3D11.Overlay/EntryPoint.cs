89dddef5fea24f607ec5ba87d2b87fcd6a2cbf95
Thierry <thierry44@gmail.com>
2018-12-09 12:38:47 -0500

    project: Add base Direct3D11 overlay template

---
+++ src/DirectX.Direct3D11.Overlay/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoreHook;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace DirectX.Direct3D11.Overlay
+{
+    public class EntryPoint : IEntryPoint
+    {
+        public EntryPoint(IContext context) { }
+
+        public void Run(IContext context)
+        {
+
+        }
+    }
+}
