88e4cd3479afccb40cc22f135f359bd27d096a0a
Thierry <thierry44@gmail.com>
2018-12-13 18:18:10 -0500

    Remove unused code from Direct3D9 hook entrypoint

--- src/DirectX.Direct3D9.Overlay/EntryPoint.cs
+++ src/DirectX.Direct3D9.Overlay/EntryPoint.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using CoreHook;
+﻿using CoreHook;
 using DirectX.Direct3D.Core;
-using SharpDX.Direct3D9;
 
 namespace DirectX.Direct3D9.Overlay
 {
@@ -24,44 +22,6 @@ namespace DirectX.Direct3D9.Overlay
         {
             _direct3DHook = new Direct3DHookModule();
             _direct3DHook.CreateHooks();
-        }
-
-
-        private void DrawFramesPerSecond(Device device)
-        {
-            try
-            {
-             
-                /*
-                var tickCount = Environment.TickCount;
-                if (Math.Abs(tickCount - _lastTickCount) > 1000)
-                {
-                    _lastFrameRate = (float)_frameCount * 1000 / Math.Abs(tickCount - _lastTickCount);
-                    _frameCount = 0;
-                    _lastTickCount = tickCount;
-                }
-                if (_framesPerSecondFont == null)
-                {
-                    _framesPerSecondFont = new Font(device, new FontDescription
-                    {
-                        Height = 20,
-                        FaceName = "Arial",
-                        Italic = false,
-                        Width = 0,
-                        MipLevels = 1,
-                        CharacterSet = FontCharacterSet.Default,
-                        OutputPrecision = FontPrecision.Default,
-                        Quality = FontQuality.ClearTypeNatural,
-                        PitchAndFamily = FontPitchAndFamily.Default | FontPitchAndFamily.DontCare,
-                        Weight = FontWeight.Bold
-                    });
-                }
-
-                _framesPerSecondFont.DrawText(null, $"{_lastFrameRate:N0} FPS", 0, 0, new ColorBGRA(244, 66, 86, 255));*/
-            }
-            catch (Exception)
-            {
-            }
         }
     }
 }
